a3c7d879fca34e393ac3cb6ebcadfa3ff333d0da
Vinayak <vinayakdeepu@gmail.com>
2020-06-20 20:53:37 +0530

    Add word score calculator

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,8 +3,9 @@ import { REWARD_TYPES } from '../service/square'
 
 export default ({ data }) => {
   const reward = REWARD_TYPES[data.reward];
+  const focus = data.focus ? 'focus' : '';
   return (
-    <div className={`square ${reward}`}>
+    <div className={`square ${reward} ${focus}`}>
       { data.value &&
         <div>
           <p className="value">{data.value}</p>
